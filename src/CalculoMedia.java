1b5c682cccca8f06989eee3b99cac34e903902cb
Developer-Ale <alexandre.ruthes@outlook.com>
2025-08-25 22:48:18 -0300

    commitando exercício de médias

--- src/CalculoMedia.java
+++ src/CalculoMedia.java
@@ -1,26 +1,33 @@
+import java.util.Scanner;
+
 public class CalculoMedia {
-
+    
     public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
 
-        Notas notas = new Notas();
-        notas.getNota1();
-        System.out.println("Sua primeira nota é: " + notas.getNota1());
-        notas.getNota2();
-        System.out.println("Sua segunda nota é: " + notas.getNota2());
-        notas.getNota3();
-        System.out.println("Sua terceira nota é: " + notas.getNota3());
-        notas.getNota4();
-        System.out.println("Sua quarta nota é: " + notas.getNota4());
+        System.out.println("Olá, vamos verificar se você passou de ano.");
+
+        System.out.println("Digite sua primeira nota: ");
+        double notaUm = s.nextDouble();
+
+        System.out.println("Digite sua segunda nota: ");
+        double notaDois = s.nextDouble();
+
+        System.out.println("Digite sua terceira nota: ");
+        double notatres = s.nextDouble();
+
+        System.out.println("Digite sua quarta nota: ");
+        double notaQuatro = s.nextDouble();
 
         double mediaFinal;
 
-        mediaFinal = ((notas.getNota1() + notas.getNota2() + notas.getNota3() + notas.getNota4()) / 4);
-        if (mediaFinal >= 6){
+        mediaFinal = ((notaUm + notaDois + notatres + notaQuatro) / 4);
+        if (mediaFinal >= 7) {
             System.out.println("Parabéns! Sua média final é: " + mediaFinal + ". Você está aprovado!");
-        } else {
+        } else if (mediaFinal >= 5) {
             System.out.println("Atenção! Sua média final é: " + mediaFinal + ". Você está em recuperação!");
+        } else {
+            System.out.println("Que pena! Sua média final é: " + mediaFinal + ". Você está reprovado!");
         }
-
     }
-
 }
